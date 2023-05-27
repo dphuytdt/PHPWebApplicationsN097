7cadafa4faece2530fb91a7917cd75ecdd71823c
dphuytdt <dphuytdt@gmail.com>
2023-05-27 16:08:38 +0700

    featurePHPN097-14 - update 404 page

--- Frontend/resources/views/errors/404.blade.php
+++ Frontend/resources/views/errors/404.blade.php
@@ -1,84 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="icon" type="image/png" href="{{ asset('images/logo.png') }}" />
-    <title>404 Not Found</title>
-    <link rel="stylesheet" href="{{asset('css/auth/404.css')}}" type="text/css"/>
-</head>
-<body>
-    
-<link href='https://fonts.googleapis.com/css?family=Carter+One' rel='stylesheet' type='text/css'>
-
-<div class="fon">
-  <div class="horizon"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
-<div class="hill"></div>
-
-<span class="not-found">Page - Not Found</span>
-<span class="page-404">ERROR 404</span>
-<div class="moon-sky"></div>
-<div class="satellite">☄</div>
-  <div class="meteores">
-							<div></div>
-							<div></div>
-							<div></div>
-						</div>
-<div class="cosmos-star">
-  <div></div>
- <div></div>
- <div></div>
- <div></div>
- <div></div>
- <div></div>
- <div></div>
-<div></div>
-</div>
-<div class="my-cat">
-    <div class="ear-l">
-        <div class="ear-fur-l"></div>
-    </div>
-    <div class="ear-r">
-        <div class="ear-fur-r"></div>
-    </div>
-<div class="head-cat">
-  <div class="tabby-cat"></div>
-  <div class="tabby-cat-1"></div>
-  <div class="tabby-cat-2"></div>
-    <div class="eye-l">
-        <div class="eye-lz"></div>
-    </div>
-    <div class="cat-nose"></div>
-    <div class="eye-r">
-        <div class="eye-rz"></div>
-    </div> 
-    <div class="muzzle-cat"></div>
-  <div class="	jaws-cat"></div>
-   <div class="whiskers">
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-   </div>
-</div>
-  <div class="body-cat"></div>
-  <div class="body-cat-1"></div>
-  <div class="paw-cat-l"><div></div></div>
-  <div class="paw-cat-r"><div></div></div>
-  <div class="tail-cat"></div>
-</div>
-<div class="rock">
-<div class="rock-mountain_1"></div>
-<div class="rock-mountain_s_1"></div>
-<div class="rock-mountain_2"></div>
-<div class="rock-mountain_s_2"></div>
-<div class="rock-mountain_3"></div>
-<div class="rock-mountain_s_3"></div>
-</div>
-</div>
-</div>
-</body>
+   <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <link rel="icon" type="image/png" href="{{ asset('images/logo.png') }}" />
+      <title>404 Not Found</title>
+      <link rel="stylesheet" href="{{asset('css/auth/404.css')}}" type="text/css"/>
+   </head>
+   <body>
+        <h1>404 Error Page #2</h1>
+        <p class="zoom-area"><b>CSS</b> animations to make a cool 404 page. </p>
+        <section class="error-container">
+        <span class="four"><span class="screen-reader-text">4</span></span>
+        <span class="zero"><span class="screen-reader-text">0</span></span>
+        <span class="four"><span class="screen-reader-text">4</span></span>
+        </section>
+        <div class="link-container">
+        <a target="_blank" href="https://www.silocreativo.com/en/creative-examples-404-error-css/" class="more-link">Visit the original article</a>
+        </div>
+   </body>
 </html>
