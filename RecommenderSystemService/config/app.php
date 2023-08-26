ed13f9e48cf9283b353b526bcdbfcdcb556c04c7
dphuytdt <dphuytdt@gmail.com>
2023-08-27 02:01:10 +0700

    try with Recommender System

--- RecommenderSystemService/config/app.php
+++ RecommenderSystemService/config/app.php
@@ -168,8 +168,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        App\Providers\RepositoryServiceProvider::class,
-        Amranidev\Laracombee\Providers\LaracombeeServiceProvider::class,
     ])->toArray(),
 
     /*
@@ -185,7 +183,6 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
-        'Laracombee' => Amranidev\Laracombee\Facades\LaracombeeFacade::class,
     ])->toArray(),
 
 ];
